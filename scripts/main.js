2318d36ec703b93189d89e7d36554582fb091b96
kieran <kieran@gmail.com>
2023-03-08 13:07:43 +0100

    Add GSAP cdn

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,30 @@
-import gsap  from 'gsap'
+// import gsap  from 'gsap'
 
 const tl = gsap.timeline({
     defaults: {
         duration: .5,
     }
-})
-
-gsap.from('nav li', {
-    duration: .5,
-    opacity: 0,
-    y: '-20%',
-    ease: 'power2.out',
-    stagger: .2
 });
+
+tl
+    .from('nav li', {
+        duration: .5,
+        opacity: 0,
+        y: '-20%',
+        ease: 'power2.out',
+        stagger: .2
+    })
+    .from('.hero--title span', {
+        y: '100%',
+        transform: 'rotate(10deg)',
+        stagger: .2
+    },
+    "<50%"
+    )
+    .to('.gallery img', {
+        'clip-path': 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
+        duration: .6,
+        stagger: .2
+    },
+    "<50%"
+    )
